574dfb48057c9eaf15031ce2763178e7f3a41a5b
athar05 <atharshakeel96@gmail.com>
2021-08-23 00:34:38 +0530

    final app

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ var textOutput = document.querySelector("#output-text")
 
 // textOutput.innerText = "Athar"
 
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslattionURL(text) {
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong. Please try again in some time")
+}
+
 function clickHandler() {
-       textOutput.innerText = "asdasdsds " + textInput.value;
+       var inputText = textInput.value;
+
+       fetch(getTranslattionURL(inputText))
+       .then(response => response.json())
+       .then(json => {
+
+        var translatedText = json.contents.translated;
+        
+        textOutput.innerText = translatedText; 
+       })   
+       .catch(errorHandler)
     };
 
 btnTranslate.addEventListener("click", clickHandler)
